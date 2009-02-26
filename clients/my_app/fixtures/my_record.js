28f1fb1fe95c3f260a790bf838bee74948da5aa5
Ray Bodenhorn <rbodenhorn@nike.local>
2009-02-25 21:29:13 -0800

    initialcommit

---
+++ clients/my_app/fixtures/my_record.js
@@ -0,0 +1,43 @@
+// ==========================================================================
+// MyApp.MyRecord Fixtures
+// ==========================================================================
+
+require('core') ;
+
+MyApp.FIXTURES = MyApp.FIXTURES.concat([
+
+  // TODO: Add your data fixtures here.
+  // All fixture records must have a unique guid and a type matching the
+  // name of your contact.  See the example below.
+
+  { guid: 1,
+          type: 'MyRecord',
+          firstName: "Michael",
+          lastName: "Scott"
+        },
+        
+        { guid: 2,
+          type: 'MyRecord',
+          firstName: "Dwight",
+          lastName: "Schrute"
+        },
+        
+        { guid: 3,
+          type: 'MyRecord',
+          firstName: "Jim",
+          lastName: "Halpert"
+        },
+        
+        { guid: 4,
+          type: 'MyRecord',
+          firstName: "Pam",
+          lastName: "Beesly"
+        },
+        
+        { guid: 5,
+          type: 'MyRecord',
+          firstName: "Ryan",
+          lastName: "Howard"
+        }
+
+]);
---
+++ clients/my_app/models/my_record.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// MyApp.MyRecord
+// ==========================================================================
+
+require('core');
+
+/** @class
+
+  (Document your class here)
+
+  @extends SC.Record
+  @author AuthorName
+  @version 0.1
+*/
+MyApp.MyRecord = SC.Record.extend(
+/** @scope MyApp.MyRecord.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
